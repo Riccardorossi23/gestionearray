9f1a002f74e5bd097c654d5fc967c3c64cc3c149
Riccardorossi23 <55913260+Riccardorossi23@users.noreply.github.com>
2019-11-19 20:04:41 +0100

    Add project files.

---
+++ array19/Program.cs
@@ -0,0 +1,24 @@
+﻿using Gestionearray;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] a = (3,4,3);
+            //int[] b = (3,4,3);
+           // Console.WriteLine("GestioneArray.uguali(a+b)");
+            //GestioneArray.leggiarray(a);
+            //GestioneArray.StampaArray(a);
+            //GestioneArray.crescente(a);
+            //GestioneArray.decrescente(a);
+            
+        }
+    }
+}
---
+++ gestionearray/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestionearray
+{
+    public class GestioneArray
+    {
+        public static void leggiarray(int[]array)
+        {
+            Console.WriteLine("inserici valori array :");
+            for(int i=0;array.Length;i++)
+            {
+                Console.WriteLine($"/tinserisci elemento{i}:");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        public static void StampaArray(int[]array)
+        {
+            for(int i=0;i<array.Length;i++)
+            {
+                int n = array[i];
+                Console.WriteLine($"array{i}={n}");
+            }
+        }
+        public static bool uguali(int[]a,int[]b)
+        {
+            if (a.Length != b.Length)
+            {
+                return false;
+            }
+            for(int i=0;i<a.Length;i++)
+                if(a[i]!=b[i])
+                {
+                    return false;
+                }
+            return true;
+        }
+        public static bool crescente(int[]array)
+        {
+            for(int i=1;i<array.Length;i++)
+            {
+                if(array[i]<array[i-1])
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+        public static bool descrescente(int[]array)
+        {
+            for(int i=1;i>array.Length;i--)
+            {
+                if(array[i]>array[i-1])
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
